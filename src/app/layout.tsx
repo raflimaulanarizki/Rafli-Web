a99db6ea1d6cef8f29b50d7b79b9467908d5f8a2
Muhamad Rafli Maulana Rizki <raflimaulanarizki8@gmail.com>
2025-09-01 16:54:44 +0000

    Tambahkan google analytic ya, ini source codenya
    <!-- Google tag (gtag.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,19 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Literata:wght@400;700&display=swap" rel="stylesheet" />
+        
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-14W8LCD7T2"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-14W8LCD7T2');
+            `,
+          }}
+        />
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
